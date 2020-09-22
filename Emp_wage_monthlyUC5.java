992a9879c9ffd3fc9515ae4c05ffdc62c1151ce6
sumitsamsharma <sharmasumit94286@gmail.com>
2020-09-22 17:12:18 +0530

    cas

---
+++ Emp_wage_monthlyUC5.java
@@ -0,0 +1,22 @@
+package Emp_info;
+
+public class Emp_wage_monthlyUC5 
+{
+	public static void main(String[] args)
+	{
+		int full_time=1;
+		int part_time=0;
+		int emp_wage_hr=20;
+		int emp_hr=0;
+		int emp_days=20;
+		int emp_wage=0;
+		double number=Math.floor(Math.random()*10)%2;
+		if(full_time== number)
+		     emp_hr=8;
+		else if(part_time== number)
+			emp_hr=4;
+		emp_wage=emp_hr*emp_wage_hr*emp_days;
+		System.out.println("Employee wage: "+emp_wage);
+	}
+
+}
